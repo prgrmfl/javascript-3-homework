45451d72843881952c2230e5d287ac0f9183cde4
prgrmfl <hrn.acc@protonmail.com>
2019-08-10 03:37:54 +0300

    Week1-Exercise-Three_Layer_Handlers

--- week-1/three-layer-handlers/reverse.js
+++ week-1/three-layer-handlers/reverse.js
@@ -1,23 +1,26 @@
 function reverse_string(str) {
-  return str;
+  let rev = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev += str[i];
+  }
+  return rev;
 }
 
-function reverse_handler() {                                      
-                     
+function reverse_handler() {
   // read and process user input
-  var to_reverse = document.getElementById('to-reverse').value;        
-                                 
+  var to_reverse = document.getElementById('to-reverse').value;
+
   // pass user input through core logic
-  
+
   /* call your logic function to reverse a string */
+  reverse_string(to_reverse);
   /* assign the result to a variable named 'reversed' */
   var reversed = reverse_string(to_reverse);
-
 
   // report result to user
   var output_field = document.getElementById('reversed');
   output_field.innerHTML = reversed;
-};
+}
 
-var reverse_button = document.getElementById("reverse-it");
-reverse_button.addEventListener("click", reverse_handler);
+var reverse_button = document.getElementById('reverse-it');
+reverse_button.addEventListener('click', reverse_handler);
--- week-1/three-layer-handlers/sort.js
+++ week-1/three-layer-handlers/sort.js
@@ -1,22 +1,75 @@
-function sort_charecters(str) {
+function sort_characters(str) {
+  if (typeof str === 'number') {
+    // split the string in letters/characters
+    str = str.split('');
+    // sorting
+    str = str.sort((a, b) => a.localeCompare(b, { numeric: true }));
+    // concatenate the characters in one string
+    str = str.join('');
+    return str;
+  }
+
+  // split the string in letters/characters
+  str = str.split('');
+  // sorting
+  str = str.sort((a, b) => a.localeCompare(b, { ignorePunctuation: true }));
+  // concatenate the characters in one string
+  str = str.join('');
   return str;
 }
 
-function sort_handler() {                                      
-                                                           
+function sort_handler() {
   // read and process user input
-  var to_sort = document.getElementById('to-sort').value;        
+  var to_sort = document.getElementById('to-sort').value;
 
   // pass user input through core logic
-  
-  /* write a logic function to sort charecters in a string */
+
+  /* write a logic function to sort characters in a string */
   /* assign the result to a variable named 'sorted' */
-  var sorted = sort_charecters(to_sort);
+  var sorted = sort_characters(to_sort);
 
   // report result to user
   var output_field = document.getElementById('sorted');
   output_field.innerHTML = sorted;
-};
+}
 
-var sort_button = document.getElementById("sort-it");
-sort_button.addEventListener("click", sort_handler);
+var sort_button = document.getElementById('sort-it');
+sort_button.addEventListener('click', sort_handler);
+
+function sort_words(str) {
+  if (typeof str === 'number') {
+    // split the string in letters/characters
+    str = str.split('');
+    // sorting
+    str = str.sort((a, b) => a.localeCompare(b, { numeric: true }));
+    // concatenate the characters in one string
+    str = str.join('');
+    return str;
+  }
+
+  // split the string in words
+  str = str.split(' ');
+  // sorting
+  str = str.sort((a, b) => a.localeCompare(b, { ignorePunctuation: true }));
+  // concatenate the characters in one string
+  str = str.join(' ');
+  return str;
+}
+
+function sort_handler_2() {
+  // read and process user input
+  var to_sort = document.getElementById('to-sort').value;
+
+  // pass user input through core logic
+
+  /* write a logic function to sort characters in a string */
+  /* assign the result to a variable named 'sorted' */
+  var sorted = sort_words(to_sort);
+
+  // report result to user
+  var output_field = document.getElementById('sorted_2');
+  output_field.innerHTML = sorted;
+}
+
+var sort_word_button = document.getElementById('sort-words');
+sort_word_button.addEventListener('click', sort_handler_2);
